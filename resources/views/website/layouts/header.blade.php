c46e9d4093ea11307394202fd6656cb82f95ebe2
Vivekpatil1913 <vivekpatilss23@gmail.com>
2023-12-25 18:52:11 +0530

    design about us page

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -55,7 +55,7 @@
           <a class="nav-link" href="#">Home </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="{route('aboutus')}">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Services</a>
--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -447,4 +447,118 @@
    </div>
 </div>
 
+<div class="container">
+    <div class="card p-3">
+		
+		<nav class="p-3">
+			<div class="nav nav-tabs our_advi_tab" id="nav-tab" role="tablist">
+				<button class="nav-link our_advi_tab_link" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Why Fibovix</button>
+				<button class="nav-link our_advi_tab_link active" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Our Advisor</button>
+				<button class="nav-link our_advi_tab_link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Working together</button>
+			</div>
+		</nav>
+        <h2 class="text-center p-3">Our <span style="color:#6246E5; padding:5px;">Advisor</span></h2>
+		<div class="tab-content p-3" id="nav-tabContent">
+			<div class="tab-pane fade" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+				<p><strong>This is some placeholder content the Home tab's associated content.</strong>
+					Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps
+					classes to control the content visibility and styling. You can use it with tabs, pills, and any
+					other <code>.nav</code>-powered navigation.</p>
+			</div>
+
+			<div class="tab-pane fade active show" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div class="main">    
+                    
+                    <div class="sub_main">
+                    <h4 class="our_advisor_h1">Guidance is just the beginning</h4>
+                        <p class="our_advisor_p">You may think your advisor’s role is limited to your portfolio however we believe it goes beyond that. Working with a Merrill advisor can help turn your hopes and dreams into a personalized financial strategy. Your advisor is your resource, and your strategy is your personal financial road map.</p>
+                    </div>
+                    <img class="our_advisor_img" src="{{ asset('website/images/f1.png') }}" width="450" height="300" alt='Responsive 2-column layout'>
+                </div>
+
+                <!--  -->
+
+                <div class="container mt-5">
+                <h2 class="text-center p-3">5 Things an <span style="color:#6246E5; padding:5px;">Advisor</span> can help you do</h2>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="card_container">
+                                <div class="card_header">
+                                    <img src="{{ asset('website/images/advisor/starg.png') }}" alt="card-image" class="card_image" />
+                                </div>
+                                <div class="card_body">
+                                    <p>Priortize your goals ans create a apersonalized finanacial strategy </p>
+                                </div>
+                                <div class="card_footer">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="card_container">
+                                <div class="card_header">
+                                    <img src="{{ asset('website/images/advisor/market.png') }}" alt="card-image" class="card_image" />
+                                </div>
+                                <div class="card_body">
+                                    <p>Priortize your goals ans create a apersonalized finanacial strategy </p>
+                                </div>
+                                <div class="card_footer">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="card_container">
+                                <div class="card_header">
+                                    <img src="{{ asset('website/images/advisor/goals.png') }}" alt="card-image" class="card_image" />
+                                </div>
+                                <div class="card_body">
+                                    <p>Priortize your goals ans create a apersonalized finanacial strategy </p>
+                                </div>
+                                <div class="card_footer">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="card_container">
+                                <div class="card_header">
+                                    <img src="{{ asset('website/images/advisor/tax_final.png') }}" alt="card-image" class="card_image" />
+                                </div>
+                                <div class="card_body">
+                                    <p>Priortize your goals ans create a apersonalized finanacial strategy </p>
+                                </div>
+                                <div class="card_footer">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4">
+                            <div class="card_container">
+                                <div class="card_header">
+                                    <img src="{{ asset('website/images/advisor/tax.png') }}" alt="card-image" class="card_image" />
+                                </div>
+                                <div class="card_body">
+                                    <p>Priortize your goals ans create a apersonalized finanacial strategy </p>
+                                </div>
+                                <div class="card_footer">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+			</div>
+
+			<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+				<p><strong>This is some placeholder content the Contact tab's associated content.</strong>
+					Clicking another tab will toggle the visibility of this one for the next.
+					The tab JavaScript swaps classes to control the content visibility and styling. You can use it with
+					tabs, pills, and any other <code>.nav</code>-powered navigation.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
 @include('website.layouts.footer')
