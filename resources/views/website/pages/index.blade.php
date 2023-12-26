b0a3679d52ce7863e1b9655f897874c647663f2a
Vivekpatil1913 <vivekpatilss23@gmail.com>
2023-12-26 17:10:13 +0530

    work on responsiveness

--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -101,9 +101,9 @@
                 <!-- Add your additional content here -->
             </div>
             <div class="container section_center">
-                <div class="row ">
+                <div class="row">
 
-                    <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card shadow-lg" style="width: 20rem; border-radius:150px 150px 0 150px; height:15rem">
                             <div class="card-body ">
                                 <img src="{{ asset('website/images/Group.png') }}" width="50px" alt="">
@@ -114,7 +114,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card shadow-lg" style="width: 20rem; border-radius:150px 150px 0 150px; height:15rem">
                             <div class="card-body ">
                                 <img src="{{ asset('website/images/Group1.png') }}" width="50px" alt="">
@@ -126,7 +126,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="card shadow-lg" style="width: 20rem; border-radius:150px 150px 0 150px; height:15rem">
                             <div class="card-body ">
                                 <img src="{{ asset('website/images/Group2.png') }}" width="50px" alt="">
@@ -269,7 +269,7 @@
         </div>
 
         <div class="row">
-            <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card shadow-lg action_circle">
                     <div class="card-body ">
                         <span
@@ -292,7 +292,7 @@
             
             </div>
 
-            <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card shadow-lg action_circle">
                     <div class="card-body ">
                         <span
@@ -312,7 +312,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="card shadow-lg action_circle">
                     <div class="card-body ">
                         <span
@@ -333,7 +333,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <div class="col-lg-3 col-md-6 col-sm-6 ">
                 <div class="card shadow-lg action_circle">
                     <div class="card-body ">
                         <span
@@ -464,7 +464,7 @@
                             You and your advisor will work together one on one to help create a strategy that’s uniquely yours.. </p>
                     </div>
                     <div class="col-sm-12 col-md-6 col-lg-6">
-                        <img src="{{ asset('website/images/why_fibovix.png') }}" width="450" height="300" alt='Responsive 2-column layout'>
+                        <img class="goal_advisor_img" src="{{ asset('website/images/why_fibovix.png') }}" width="450" height="300" alt='Responsive 2-column layout'>
                     </div>
                 </div>
 			</div>
@@ -606,5 +606,120 @@
 </div>
 
 
+
+<!-- Start contact-background -->
+<section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-md-12">
+                    <div class="logo image-fluid">
+                        <img src="{{ asset('website/images/contact-background.jpg') }}" width="100%" ;>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End contact-background -->
+
+    <!-- Start Cards -->
+    <div class="container mb-md-4">
+        <div class="row g-0">
+            <div class="col-md-12">
+                <div class="row g-3 justify-content-center">
+                    <div class="col-md-12 col-lg-8">
+                        <div class="row">
+                            <div class="col-md-4 col-lg-4 text-center d-flex align-items-stretch">
+                                <div class="services">
+                                    <div class="icon d-flex align-items-center justify-content-center"><span>
+                                            <i><img src="{{ asset('website/images/Group 1000003610.png') }}" alt=""></i></span>
+                                    </div>
+                                    <div class="text">
+                                        <h2>Office</h2>
+                                        <p>Express towers, Marine Dr, Nariman Point, Mumbai, Maharashtra 400021 </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-lg-4 text-center d-flex align-items-stretch">
+                                <div class="services">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <span<i><img src="{{ asset('website/images/Group 1000003611.png') }}" alt=""></i></span>
+                                    </div>
+                                    <div class="text">
+                                        <h2>Email</h2>
+                                        <p>info@fibovix.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-lg-4 text-center d-flex align-items-stretch">
+                                <div class="services">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <span<i><img src="{{ asset('website/images/Group 1000003612.png') }}" alt=""></i> </span>
+                                    </div>
+                                    <div class="text">
+                                        <h2>Phone</h2>
+                                        <p>6262757562 </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+    <!-- End Cards -->
+
+    <!-- start Map -->
+    <section>
+        <div class="container" style="position: relative;">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 d-flex justify-content-center">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d22097.967898937644!2d73.78348365959188!3d19.987863371093294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1703580760467!5m2!1sen!2sin"
+                        width="800" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Map -->
+
+    <!--Start form -->
+    <section class=" d-flex justify-content-center">
+        <div class="contact-form col-md-4 background-color p-5">
+        <form class="row g-3 needs-validation" novalidate>
+            <div class="col-md-6">
+                <label for="validationCustom01" class="form-label">Name</label>
+                <input type="text" class="form-control" id="validationCustom01" required>
+
+            </div>
+            <div class="col-md-6">
+                <label for="validationCustom02" class="form-label">Email</label>
+                <input type="text" class="form-control" id="validationCustom02" required>
+            </div>
+
+            <div class="col-md-6">
+                <label for="validationCustom03" class="form-label">Phone</label>
+                <input type="text" class="form-control" id="validationCustom03" required>
+            </div>
+            <div class="col-md-6">
+                <label for="validationCustom03" class="form-label">Subject</label>
+                <input type="text" class="form-control" id="validationCustom03" required>
+            </div>
+            <div class="col-md-12">
+                <label for="message" class="form-label">Message</label>
+                <textarea class="form-control" id="message" rows="4" placeholder="Enter your message"></textarea>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center mt-3"><br>
+                <button class="btn btn-primary" type="submit">Send Message</button>
+            </div>
+        </form>
+
+        </div>
+    </section>
+    <!-- End form -->
 
 @include('website.layouts.footer')
