7c29b6d85ef77a4f317763e514c3c5d54f0128e5
Satish <satishaurange@sumagoinfotech.com>
2024-01-01 18:53:19 +0530

    changes for the servics updated one updated new

--- resources/views/website/pages/services.blade.php
+++ resources/views/website/pages/services.blade.php
@@ -50,14 +50,14 @@
                 <div class="col-md-9">
                     <div class="tab-content p-3" id="nav-tabContent">
                         <div class="row d-flex gallery" id="gallary_data">
-                            <!-- @forelse ($all_services_details as $key=>$itemNew)
-                            <div class="tab-pane fade active @if($key==0) {{'show'}} @endif"
+                            @forelse ($all_services_details as $key=>$itemNew)
+                           <!--  <div class="tab-pane fade active @if($key==0) {{'show'}} @endif"
                                 id="data_id_{{$itemNew['service_details_id']}}" role="tabpanel"
                                 aria-labelledby="id_{{$itemNew['service_details_id']}}"> -->
 
                                 <div class="row row-cols-1 row-cols-md-4 g-3">
                                     @forelse ($all_services_details as $key=>$item)
-                                    <!-- @if($itemNew['service_details_id'] === $item['service_details_id']) -->
+                                    @if($itemNew['service_details_id'] === $item['service_details_id'])
                                     <div class="col-md-6 col-lg-4 col-sm-12">
                                         <div class="card article_card_container h-100">
                                             <img src="{{ Config::get('DocumentConstant.SERVICES_VIEW') }}{{ $item['image'] }}"
@@ -72,7 +72,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <!-- @endif -->
+                                    @endif
                                     @empty
                                     <div class="alert alert-primary" role="alert">
                                         No Data Found
@@ -81,10 +81,10 @@
                                 </div>
 
                             </div>
-                            <!-- @empty
+                            @empty
                             {{'No Categries found'}}
                             @endforelse
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
